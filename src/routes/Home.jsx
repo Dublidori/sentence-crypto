bc89bdc19047b403a046c50515844c375f6f218d
Dublidori <miguelantunes123cr@gmail.com>
2024-02-19 18:15:12 +0000

    Added template

---
+++ src/routes/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
+import Sentence from '../components/Sentence'
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar />
+            <Sentence />
+            <Footer />
+        </div>
+    )
+}
+
+export default Home
