ed959c448bf05cb23c3912ee5a6288ee3a764790
Dublidori <miguelantunes123cr@gmail.com>
2024-02-24 20:15:50 +0000

    wagmiConfig changes

--- src/config/wagmiConfig.js
+++ src/config/wagmiConfig.js
@@ -1,20 +1,16 @@
 import { http, createConfig } from 'wagmi';
-import { base, mainnet, optimism, sepolia } from 'wagmi/chains';
+import { base, mainnet, optimism, sepolia, polygon } from 'wagmi/chains';
 import { injected, metaMask, safe, walletConnect } from 'wagmi/connectors';
 
 const projectId = '36cac5156311413d288cbb0faaa4f367';
 
 export const wagmiConfig = createConfig({
-  chains: [mainnet, base],
+  chains: [mainnet, sepolia],
   connectors: [
-    injected(),
     walletConnect({ projectId }),
-    metaMask(),
-    safe(),
   ],
   transports: {
     [mainnet.id]: http(),
     [sepolia.id]: http(),
-    [base.id]: http(),
   },
 })
