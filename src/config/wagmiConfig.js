7df653821b8a10d760f0a16abbe580ec9a1d9290
Dublidori <miguelantunes123cr@gmail.com>
2024-02-28 00:24:30 +0000

    Wagmi Config

--- src/config/wagmiConfig.js
+++ src/config/wagmiConfig.js
@@ -8,6 +8,7 @@ export const wagmiConfig = createConfig({
   chains: [mainnet, sepolia],
   connectors: [
     walletConnect({ projectId }),
+    injected({ target: 'metaMask' }), // This is important for metamask behaviors. Font: Wagmi Docs recent version
   ],
   transports: {
     [mainnet.id]: http(),
