df1fab1f3393bf425a6b8c439388b8b92bef8ef3
Dublidori <miguelantunes123cr@gmail.com>
2024-04-11 22:17:36 +0100

    Working on ui

--- src/components/Leaderboard/Leaderboard.jsx
+++ src/components/Leaderboard/Leaderboard.jsx
@@ -1,22 +1,39 @@
 // components/Leaderboard.jsx
 import React from 'react';
-import { Box, Typography, List, ListItem, Paper } from '@mui/material';
+import { Grid, Typography, List, ListItem, Paper } from '@mui/material';
 
 const Leaderboard = ({ entries }) => {
-  return (
-    <Paper sx={{ p: 2, backgroundColor: '#222', mt: 2, borderColor: 'primary.main', borderWidth: 3, borderStyle: 'solid' }}>
-      <Typography variant="h4" component="h2" align="center" color="primary" gutterBottom>
-        Top Sentences
-      </Typography>
-      <List>
-        {entries.map((entry, index) => (
-          <ListItem key={index} sx={{ borderBottom: '1px dashed #0f0', py: 1, color: '#0f0' }}>
-            "{entry.text}" - {entry.amount} ETH
-          </ListItem>
-        ))}
-      </List>
-    </Paper>
-  );
+    return (
+        <Paper sx={{ p: 2, backgroundColor: '#222', mt: 2, borderColor: 'primary.main', borderWidth: 3, borderStyle: 'solid', overflow: 'hidden' }}>
+            <Typography variant="h4" component="h2" align="center" color="primary" gutterBottom>
+                Top Sentences
+            </Typography>
+            <List>
+                <Grid container>
+                    {entries.map((entry, index) => (
+                        <ListItem key={index} sx={{ borderBottom: '1px dashed #0f0', py: 1, color: '#0f0', pt: 3 }}>
+                            <Grid item sm={3}  sx={{ justifyContent: 'flex-start', display: 'flex' }}>
+                                <Typography variant="h7" component="h2" align="center" color="primary" gutterBottom>
+                                    {entry.text}
+                                </Typography>
+                            </Grid>
+                            <Grid item sm={3} sx={{ justifyContent: 'flex-end', display: 'flex' }}>
+                                <Typography variant="h7" component="h2" align="center" color="primary" gutterBottom>
+                                    {entry.amount} ETH
+                                </Typography>
+                            </Grid>
+                            <Grid item sm={6} sx={{ justifyContent: 'flex-end', display: 'flex' }}>
+                                <Typography variant="h7" component="h2" align="center" color="primary" gutterBottom>
+                                    {entry.wallet}
+                                </Typography>
+                            </Grid>
+
+                        </ListItem>
+                    ))}
+                </Grid>
+            </List>
+        </Paper>
+    );
 };
 
 export default Leaderboard;
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -8,7 +8,11 @@ import matrix from "../assets/matrix.gif";
 
 // This data would likely come from state or props in a real application
 const leaderboardEntries = [
-    { text: 'Cryptography Enthusiast', amount: 4.2 },
+    { text: 'Cryptography Enthusiast', amount: 4.2, wallet: '0x85a2b7609Fc92181a2A5fb565D2895B5a1D7835C' },
+    { text: 'Top G', amount: 2.2, wallet: '0x85a2b7609Fc92181a2A5fb565D2895B5a1D7835C' },
+    { text: 'Woke', amount: 1.2, wallet: '0x85a2b7609Fc92181a2A5fb565D2895B5a1D7835C' },
+    { text: 'BITCOIN TO 200k', amount: 0.2, wallet: '0x85a2b7609Fc92181a2A5fb565D2895B5a1D7835C' },
+    { text: 'crypto is fun', amount: 0.02, wallet: '0x85a2b7609Fc92181a2A5fb565D2895B5a1D7835C' },
     // ...more entries
 ];
 
